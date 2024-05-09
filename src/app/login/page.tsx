5b3d3e6855135eb780a33800e1efab52e59a4db3
LastKnight12 <vovaua27@gmail.com>
2024-05-10 00:45:53 +0300

    modified:   next.config.js (google auth) 	modified:   src/app/login/page.tsx (google auth) 	modified:   src/env.js (google auth) 	modified:   src/server/auth.ts (google auth)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,18 +2,25 @@
 
 import { Button } from "@nextui-org/button";
 import { signIn } from "next-auth/react";
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaGoogle } from "react-icons/fa";
 
 
 export default function Sigin() {
   return (
-    <div className=" flex h-full w-full items-center justify-center">
+    <div className=" flex flex-col gap-2 h-full w-full items-center justify-center">
       <Button
         variant="flat"
         onClick={() => signIn("discord", { redirect: true, callbackUrl: `http://${process.env.NEXTAUTH_URL}` })}
         color="primary"
       >
         Login with Discord <FaDiscord />
+      </Button>
+      <Button
+        variant="flat"
+        onClick={() => signIn("google", { redirect: true, callbackUrl: `http://${process.env.NEXTAUTH_URL}` })}
+        color="primary"
+      >
+        Login with Google <FaGoogle />
       </Button>
     </div>
   );
