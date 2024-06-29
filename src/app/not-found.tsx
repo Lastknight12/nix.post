09832c8d59a931012393f680e3273deb31de2c14
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2024-06-29 12:16:14 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="relative h-[calc(100vh-116px)] w-full">
-      <div className="pointer-events-none absolute left-0 top-0 -z-[1] flex h-full w-full items-center justify-center text-[290px] light light:text-[#f3f3f3] dark:text-[#212121] max-sm:text-[165px] max-[345px]:text-[165px]">
+      <div className="pointer-events-none absolute left-0 top-0 -z-[1] flex h-full w-full items-center justify-center text-[290px] light light:text-[#f3f3f3] dark:text-[#212121] max-sm:text-[190px] max-[345px]:text-[165px]">
         404
       </div>
       <div className="flex h-full w-full flex-col items-center justify-center light light:text-black dark:text-white">
